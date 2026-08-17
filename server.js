56b034dde102a9e8f56f824d442aeb4c1b4b93e5
boostempireapp-ux <boostempire.app@gmail.com>
2026-08-16 23:56:51 -0700

    Use environment variable for server port

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const fs = require('fs');
 
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 if (!fs.existsSync('./data')) fs.mkdirSync('./data');
 
